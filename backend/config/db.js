c2a060d7f94037ed725f0c273618f219003380c2
Shivam <codewithdh@gmail.com>
2025-12-12 17:34:05 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,12 @@ import mysql from "mysql2";
 import express from "express";
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST ,
-    user: process.env.DB_USER , 
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME ,
-    port: process.env.DB_PORT 
+    host: "localhost",
+    user: 'root',
+    password: 'Shivam.123',
+    database: 'meat_hub_db',
+    waitForConnections: true,
+    connectionLimit: 10
   });
 
 // Convert connection to use promises
